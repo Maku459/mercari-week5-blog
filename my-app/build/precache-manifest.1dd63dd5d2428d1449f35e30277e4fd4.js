46b0540680e553d12e78b1e7da91e35220f300ff
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2020-06-30 11:29:17 +0900

    build file upload

---
+++ my-app/build/precache-manifest.1dd63dd5d2428d1449f35e30277e4fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c212410dd78590e83f09516a311d353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca39439fa6805f11047",
+    "url": "/static/css/main.d809efe4.chunk.css"
+  },
+  {
+    "revision": "3b7f0089c1519c433cb5",
+    "url": "/static/js/2.f9114d9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f9114d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca39439fa6805f11047",
+    "url": "/static/js/main.f6e6759c.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
